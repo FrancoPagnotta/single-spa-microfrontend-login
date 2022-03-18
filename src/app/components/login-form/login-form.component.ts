21d414088a9dea7f6878657a43fdcc58bc9e45b1
Franco Pagnotta <francoapagnotta@gmail.com>
2022-03-17 23:53:54 -0300

    Create reactive form module

--- src/app/components/login-form/login-form.component.ts
+++ src/app/components/login-form/login-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login-form',
@@ -7,9 +8,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginFormComponent implements OnInit {
 
-  constructor() { }
+  form!: FormGroup
+
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    this.createForm()
+  }
+
+  createForm(): void {
+    this.form = this.formBuilder.group({
+      email: ["", Validators.required],
+      password: ["", Validators.required]
+    })
+  }
+
+  get invalidEmailControl(): boolean {
+    return this.form.get("email")!.touched && this.form.get("email")!.invalid
+  }
+
+  get invalidPasswordControl(): boolean {
+    return this.form.get("password")!.touched && this.form.get("password")!.invalid
+  }
+
+  sendData(): void {
+    console.log(this.form.value)
+    if (this.form.valid) {
+      console.log("valid form, go to the main")
+    } else {
+      console.log("invalid form")
+    }
   }
 
 }
