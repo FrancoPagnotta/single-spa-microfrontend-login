8a560db5bad1d0775e40e3914c95d3c874249dea
Franco Pagnotta <francoapagnotta@gmail.com>
2022-03-17 22:57:49 -0300

    Create login form component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { APP_BASE_HREF } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
+import { LoginFormComponent } from "./components/login-form/login-form.component";
 import { EmptyRouteComponent } from "./empty-route/empty-route.component";
 
 const routes: Routes = [
+  { path: "", component: LoginFormComponent },
   { path: "**", component: EmptyRouteComponent }
 ];
 
